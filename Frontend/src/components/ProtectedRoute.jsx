32f933489618653b9bb8631871be6d174172ef58
OM JADHAV <137640443+omjadhav02@users.noreply.github.com>
2025-10-08 17:11:20 +0530

    Delete Frontend directory

--- Frontend/src/components/ProtectedRoute.jsx
+++
@@ -1,8 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-function ProtectedRoute({ children }) {
-  const isAdmin = localStorage.getItem("isAdmin") === "true";
-  return isAdmin ? children : <Navigate to="/admin/login" />;
-}
-
-export default ProtectedRoute;
