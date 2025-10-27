28685eb0e701812cf50880fc1a362ba6a2f30728
OM JADHAV <omjadhav1053@gmail.com>
2025-10-27 14:11:50 +0530

    updates

--- server/server.js
+++ server/server.js
@@ -16,10 +16,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ 
-    origin: "http://localhost:5173",
-    credentials: true,
-}));
+app.use(cors());
 
 const PORT = process.env.PORT || 2000;
 
